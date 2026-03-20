bbc3e6de18d7bbaa5a0fd2c8dfb9b19584434569
adrioui <adrifdlh@gmail.com>
2026-03-20 22:08:55 +0700

    solve(01,02): complete 7458 chip and vector0 exercises

--- hdlbits/exercises/01_basics/7458.v
+++ hdlbits/exercises/01_basics/7458.v
@@ -36,8 +36,8 @@
 //    │         ├──►[AND]──┘                                    │
 //    │  p2d ──►┘                                               │
 //    │                                                         │
-//    │  p1y = (p1a & p1b & p1c) | (p1d & p1e & p1f)           │
-//    │  p2y = (p2a & p2b) | (p2c & p2d)                       │
+//    │  p1y = (p1a & p1b & p1c) | (p1d & p1e & p1f)            │
+//    │  p2y = (p2a & p2b) | (p2c & p2d)                        │
 //    └─────────────────────────────────────────────────────────┘
 //
 // ──────────────────────────────────────────────────────────────────────────
@@ -45,12 +45,12 @@
 // You need to drive two signals (`p1y` and `p2y`) with a value.
 // ──────────────────────────────────────────────────────────────────────────
 
-// I AM NOT DONE
 
 module top_module ( 
     input p1a, p1b, p1c, p1d, p1e, p1f,
     output p1y,
     input p2a, p2b, p2c, p2d,
     output p2y );
-
+    assign p1y = (p1a & p1b & p1c) | (p1d & p1e & p1f);
+    assign p2y = (p2a & p2b) | (p2c & p2d);
 endmodule
