eb28e36a3148046c6b3728271e7e09dc2b7bfb5e
adrioui <adrifdlh@gmail.com>
2026-03-25 18:49:42 +0700

    solve(03): chain three DFFs as shift register

--- hdlbits/exercises/03_modules/module_shift.v
+++ hdlbits/exercises/03_modules/module_shift.v
@@ -28,15 +28,17 @@
 //    │        │  d   q │    │  d   q │    │  d   q │                  │
 //    │        │  clk   │    │  clk   │    │  clk   │                  │
 //    │        └───┬────┘    └───┬────┘    └───┬────┘                  │
-//    │            │             │             │                        │
+//    │            │             │             │                       │
 //    │  clk ──────┴─────────────┴─────────────┘                       │
 //    │                                                                │
 //    └────────────────────────────────────────────────────────────────┘
 //
 // ──────────────────────────────────────────────────────────────────────────
 
-// I AM NOT DONE
 
 module top_module ( input clk, input d, output q );
-
+wire wire1, wire2;
+my_dff instance1(.clk(clk), .d(d), .q(wire1));
+my_dff instance2(.clk(clk), .d(wire1), .q(wire2));
+my_dff instance3(.clk(clk), .d(wire2), .q(q));
 endmodule
