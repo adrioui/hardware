33e9b7fb43b54869cd8e29ee466492fce5f4d34e
adrioui <adrifdlh@gmail.com>
2026-03-26 00:12:59 +0700

    solve(03): implement 8-bit shift register with mux selector

--- hdlbits/exercises/03_modules/module_shift8.v
+++ hdlbits/exercises/03_modules/module_shift8.v
@@ -54,5 +54,20 @@ module top_module (
     input [1:0] sel, 
     output [7:0] q 
 );
+wire [7:0] wire1;
+wire [7:0] wire2;
+wire [7:0] wire3;
 
-endmodule
+my_dff8 instance1 (.clk(clk), .d(d), .q(wire1));
+my_dff8 instance2 (.clk(clk), .d(wire1), .q(wire2));
+my_dff8 instance3 (.clk(clk), .d(wire2), .q(wire3));
+
+always @(*) begin
+  case (sel)
+    2'b00: q = d;
+    2'b01: q = wire1;
+    2'b10: q = wire2;
+    2'b11: q = wire3;
+  endcase
+end
+endmodule 
