4fb577ce2104b5fc2d0228f43fb0d970a37039f9
adrioui <adrifdlh@gmail.com>
2026-03-26 12:37:55 +0700

    solve(03): implement 32-bit adder with two chained add16 modules

--- hdlbits/exercises/03_modules/module_add.v
+++ hdlbits/exercises/03_modules/module_add.v
@@ -36,12 +36,25 @@
 //
 // ──────────────────────────────────────────────────────────────────────────
 
-// I AM NOT DONE
 
 module top_module (
     input  [31:0] a,
     input  [31:0] b,
     output [31:0] sum
 );
+  wire wire1;
 
+  add16 instance1 (
+    .a(a[15:0]),
+    .b(b[15:0]),
+    .cin(1'b0),
+    .cout(wire1),
+    .sum(sum[15:0])
+  );
+  add16 instance2 (
+    .a(a[31:16]),
+    .b(b[31:16]),
+    .cin(wire1),
+    .sum(sum[31:16])
+  );
 endmodule
