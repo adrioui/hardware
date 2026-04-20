d74c56af91e44bdbc029cb9237736add73bf433a
adrioui <adrifdlh@gmail.com>
2026-04-20 23:28:02 +0700

    feat(exercises): solve module_addsub, always_case, always_case2, always_casez

--- hdlbits/exercises/04_procedures/always_case.v
+++ hdlbits/exercises/04_procedures/always_case.v
@@ -34,7 +34,6 @@
 //
 // ──────────────────────────────────────────────────────────────────────────
 
-// I AM NOT DONE
 
 // synthesis verilog_input_version verilog_2001
 module top_module (
@@ -45,11 +44,19 @@ module top_module (
     input [3:0] data3,
     input [3:0] data4,
     input [3:0] data5,
-    output reg [3:0] out   );
-//
+    output reg [3:0] out
+);
 
-    always@(*) begin  // This is a combinational circuit
-        case(...)
-    end
+  always @(*) begin  // This is a combinational circuit
+    case (sel)
+      3'd0: out = data0;
+      3'd1: out = data1;
+      3'd2: out = data2;
+      3'd3: out = data3;
+      3'd4: out = data4;
+      3'd5: out = data5;
+      default: out = 0;
+    endcase
+  end
 
 endmodule
